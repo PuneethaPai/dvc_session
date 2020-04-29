9b30b3dd11f711e9a3f123f1fcb43b94fbdb025f
Puneetha Pai <Puneetha_Pai@external.mckinsey.com>
2020-04-29 20:28:59 +0530

    DVC: Refactor and update pipeline with proper metrics data

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,12 +1,7 @@
-import _pickle as cpickle
-import os
-
-import matplotlib.pyplot as plt
-import pandas as pd
 import plac
-from dagshub import dagshub_logger
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import plot_confusion_matrix
+
+from utils import evaluate_model, print_results, save_results, log_experiment, read_data
 
 
 @plac.annotations(
@@ -14,31 +9,18 @@ from sklearn.metrics import plot_confusion_matrix
     out_path=("Path to save trained Model", "option", "o", str)
 )
 def main(data_path='data/features/', out_path='data/models/logistic/'):
-    train = pd.read_csv(f'{data_path}train.csv')
-    test = pd.read_csv(f'{data_path}test.csv')
+    X_test, X_train, y_test, y_train = read_data(data_path)
 
-    X_train, y_train = train.drop(columns=['class']), train['class']
-    X_test, y_test = test.drop(columns=['class']), test['class']
-
-    model = LogisticRegression(n_jobs=4)
+    name = 'LogisticRegression'
+    model = LogisticRegression(penalty='l2', n_jobs=4)
     model.fit(X_train, y_train)
 
-    if not os.path.isdir(out_path):
-        os.makedirs(out_path)
-    with open(f'{out_path}model.pkl', 'wb+') as fp:
-        cpickle.dump(model, fp)
+    accuracy, c_matrix, fig = evaluate_model(model, X_test, y_test)
+    print_results(accuracy, c_matrix, name)
 
-    cmd = plot_confusion_matrix(model, X_test, y_test, cmap=plt.cm.Reds)
-    cmd.figure_.savefig(f'{out_path}confusion_matrix.svg', format='svg')
-    c_matrix = cmd.confusion_matrix
-    accuracy = model.score(X_test, y_test)
-
-    print(f'Finished Training LogisticRegressionModel:\nStats:')
-    print(f'\tConfusion Matrix:\n{c_matrix}')
-    print(f'\tModel Accuracy: {accuracy}')
-    with dagshub_logger(metrics_path=f'{out_path}metrics.csv', hparams_path=f'{out_path}params.yml') as logger:
-        logger.log_hyperparams(penalty='l2')
-        logger.log_metrics(accuracy=accuracy, confusion_matrics=c_matrix)
+    save_results(out_path, model, fig)
+    log_experiment(out_path, params=dict(name=name, penalty='l2'),
+                   metrics=dict(accuracy=accuracy, confusion_matrics=c_matrix))
 
 
 if __name__ == '__main__':
---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from dagshub import dagshub_logger
+from joblib import dump
+from sklearn.metrics import plot_confusion_matrix
+
+
+def log_experiment(out_path, params: dict, metrics: dict):
+    with dagshub_logger(metrics_path=f'{out_path}metrics.csv', hparams_path=f'{out_path}params.yml') as logger:
+        logger.log_hyperparams(params=params)
+        logger.log_metrics(metrics=metrics)
+
+
+def print_results(accuracy, c_matrix, model_name=''):
+    print(f'Finished Training {model_name}:\nStats:')
+    print(f'\tConfusion Matrix:\n{c_matrix}')
+    print(f'\tModel Accuracy: {accuracy}')
+
+
+def evaluate_model(model, X_test, y_test):
+    cmd = plot_confusion_matrix(model, X_test, y_test, cmap=plt.cm.Reds)
+    c_matrix = cmd.confusion_matrix
+    accuracy = model.score(X_test, y_test)
+    return accuracy, c_matrix, cmd.figure_
+
+
+def save_results(out_path, model, fig):
+    if not os.path.isdir(out_path):
+        os.makedirs(out_path)
+    dump(model, f'{out_path}model.gz')
+
+    fig.savefig(f'{out_path}confusion_matrix.svg', format='svg')
+
+
+def read_data(data_path):
+    train = pd.read_csv(f'{data_path}train.csv')
+    test = pd.read_csv(f'{data_path}test.csv')
+    X_train, y_train = train.drop(columns=['class']), train['class']
+    X_test, y_test = test.drop(columns=['class']), test['class']
+    return X_test, X_train, y_test, y_train
